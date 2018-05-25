cba502028eaf47d17f384dd7918822ec0b15e03d
Anders Einar Hilden <hildenae@gmail.com>
2018-05-25 22:07:49 +0200

    Change default storage path and system wide config file path

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #MCN_TOOLS have been moved to:
-source "$HOME/.config/makecleanno.conf"
+source "$HOME/.mcn.conf"
 TORRENT_PATH="$PWD/torrents"
-STORAGE_PATH="/media/en/urlteam-download"
+STORAGE_PATH="$PWD/urlteam-download"
 THREADS=8
 DOMAINS="mcn-source-urlteam.list"
