9701a45661436c6024fca98b427e51b801e1dfcf
Anders Einar Hilden <hildenae@gmail.com>
2018-06-08 20:40:45 +0200

    Print size of five larges torrents in addition to total size of all torrents

--- est_torrent_dw_size.sh
+++ est_torrent_dw_size.sh
@@ -1,12 +1,27 @@
 #! /bin/bash
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${SOURCE_DIR}"
+cd "$SOURCE_DIR" || exit
 
 source config.sh
 
-find "$TORRENT_PATH" -type f -name '*.torrent' | \
-  while read F; do 
-      aria2c --show-files=true --dir="./download" --torrent-file $F | grep 'Total Length:'; 
-  done | \
-      cut -d'(' -f2 | tr -d ')' | tr -d , | tr '\n' '+' | head -c -1 | \
-      sed -e 's#^#(#' -e 's#$#)/1024/1024/1024\n#' | bc | tr -d '\n'; echo "GB (gibibyte)"
+SIZES="$(find "$TORRENT_PATH" -type f -name '*.torrent' | \
+  while read -r F; do
+    TL=$(aria2c --show-files=true --dir="./download" --torrent-file "$F" | \
+      grep 'Total Length:' | cut -d'(' -f2 | tr -d ')');
+    echo "$TL $(basename "$F") ";
+  done | sort -n)"
+
+echo >&2 "[INFO] Five largest:"
+echo "$SIZES" | tail -5 | tac | while read -r LARGE; do
+  SIZE="$(echo "$LARGE" | awk '{ print $1 }' | \
+    tr -d , | tr '\n' '+' | head -c -1 | \
+    sed -e 's#^#(#' -e 's#$#)/1024/1024\n#' | \
+    bc | tr -d '\n'; echo " MiB (mebibyte)")"
+  NAME="$(echo "$LARGE" | awk '{ print $2 }')"
+  echo "$NAME $SIZE"
+done
+echo -n >&2 "[INFO] Total: "
+echo "$SIZES" | awk '{ print $1 }' | \
+  tr -d , | tr '\n' '+' | head -c -1 | \
+  sed -e 's#^#(#' -e 's#$#)/1024/1024/1024\n#' | \
+  bc | tr -d '\n'; echo " GiB (gibibyte)"
