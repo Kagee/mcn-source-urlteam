c6773676d63f4ff8abf8485a99a5002516a6a490
Anders Einar Hilden <hildenae@gmail.com>
2018-06-08 20:53:20 +0200

    More folders in config and explanations of use

--- config.sh
+++ config.sh
@@ -1,6 +1,11 @@
 #MCN_TOOLS have been moved to:
 source "$HOME/.mcn.conf"
+# Contains all *.torrent files
 TORRENT_PATH="$PWD/torrents"
-STORAGE_PATH="$PWD/urlteam-download"
+# Temporarily stores all torrent data, and the resulting ".no" raw extracts
+STORAGE_PATH="$PWD/download"
+# Contains extracts of all urls containing ".no" from torrents
+# Also used as temporary dir for extracted torrents
+CACHE_PATH="$PWD/no_extract_cache"
 THREADS=8
 DOMAINS="mcn-source-urlteam.list"
