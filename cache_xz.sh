64cdff69c5b002492b3d2b44139481d038777ef8
Anders Einar Hilden <hildenae@gmail.com>
2018-06-17 01:05:20 +0200

    better messages, skip if cache exsists

--- cache_xz.sh
+++ cache_xz.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ME="$(basename "${BASH_SOURCE[0]}")"
 cd "${SOURCE_DIR}"
 source config.sh
 
 CACHEDIR="$1"
 FILENAME="$2"
-MD=$(md5sum "$FILENAME" | cut -d' ' -f 1);
-if [ ! -e "${CACHEDIR}/${MD}" ]; then
-    echo "[INFO] No dotno-cache for ${FILENAME} ($MD)" 1>&2;
+MD="$(md5sum "$FILENAME" | cut -d' ' -f 1).xz";
+if [ ! -e "$CACHEDIR/$MD" ]; then
+  INFO "No dotno-cache for $FILENAME ($MD) [$ME]"
+  echo -n "[INFO] "
   xz --decompress --stdout "$FILENAME" | \
-      grep -a -F '.no' > "${CACHEDIR}/${MD}.tmp"; \
-          mv "${CACHEDIR}/${MD}.tmp" "${CACHEDIR}/${MD}" && \
-          echo "${MD}";
+    grep -a -F '.no' | xz --compress --stdout > "$CACHEDIR/$MD.tmp";
+  mv "$CACHEDIR/$MD.tmp" "$CACHEDIR/$MD" && \
+    echo "$MD";
 else
-    echo "[INFO] Found dotno-cache for ${FILENAME} ($MD)" 1>&2;
-    echo "${MD}";
+    VERB "Found dotno-cache for $FILENAME ($MD) [$ME]"
+    echo "$MD";
 fi
